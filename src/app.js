266610b1f61506647e49df35867b514c4bde7e44
nathanremond <perso.nathan.remond@gmail.com>
2025-11-13 10:56:52 +0100

    feat: Create userRoutes and call it in app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.use(express.json());
 
+const userRoutes = require("./routes/userRoutes.js");
+app.use("/api/users", userRoutes);
+
 app.get("/", (req, res) => res.json({ message: "API OK" }));
 
 export default app;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,1 +1,13 @@
+import express from "express";
 import UserController from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.get("/", UserController.listUsers);
+router.get("/:id", UserController.getUser);
+router.post("/register", UserController.register);
+router.post("/login", UserController.login);
+router.put("/:id", UserController.updateUser);
+router.delete("/:id", UserController.deleteUser);
+
+export default router;
